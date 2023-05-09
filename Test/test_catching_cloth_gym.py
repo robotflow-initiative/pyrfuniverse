df9175ff87529d533d2bc53987294a583d705f62
yanbing.zhou <wrzh26252@outlook.com>
2023-05-09 10:31:54 +0800

    change URDF path

--- Test/test_catching_cloth_gym.py
+++ Test/test_catching_cloth_gym.py
@@ -1,6 +1,6 @@
 from pyrfuniverse.envs.multi_physics import KinovaGen2CatchingClothEnv
 
-env = KinovaGen2CatchingClothEnv(urdf_file='../Mesh/jaco/j2s7s300_gym.urdf')
+env = KinovaGen2CatchingClothEnv(urdf_file='../URDF/jaco/j2s7s300_gym.urdf')
 env.reset()
 
 while 1:
