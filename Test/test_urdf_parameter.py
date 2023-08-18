bb7ddcd2b1a3d2d0d7cfa389d7aaab7c7e9fc5b1
yanbing.zhou <wrzh26252@outlook.com>
2023-08-18 16:53:46 +0800

    update

--- Test/test_urdf_parameter.py
+++ Test/test_urdf_parameter.py
@@ -1,9 +1,12 @@
 from pyrfuniverse.envs.base_env import RFUniverseBaseEnv
+import pyrfuniverse.attributes as attr
 import os
 
 env = RFUniverseBaseEnv()
-kinova = env.LoadURDF(path=os.path.abspath('../URDF/Franka/panda.urdf'), axis='z')
-kinova.SetTransform(position=[0, 0, 0])
-env.ShowArticulationParameter(kinova.id)
+robot = env.LoadURDF(path=os.path.abspath('../URDF/Franka/panda.urdf'), axis='z')
+# robot = env.InstanceObject('franka_panda', attr_type=attr.ControllerAttr)
+robot.SetTransform(position=[0, 0, 0])
+robot.EnabledNativeIK(False)
+env.ShowArticulationParameter(robot.id)
 
 env.Pend()
